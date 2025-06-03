01dc8c1b8599632a1820ffdaa087580ffa4fa836
Mark Devenyi <markdevenyidev@gmail.com>
2025-06-02 22:10:45 -0400

    feat(sha2): refactor `sha256.asm` with macros and add partial hash calculation
    This commit introduces the `shani_load` and `shani_update` macros for
    streamlined SHA-256 round scheduling using SHA-NI extension. The hash
    for the 512-bit block is successfully calculated, but the final hash
    state addition and digest extraction are not yet implemented.
    
    I hereby want to thank Lucas Hufnagel, Matheus Xavier and Pavel Skripkin
    for their help. OpenSSL and LibreSSL both provided excellent resources
    as well.

--- src/sha2/sha256.asm
+++ src/sha2/sha256.asm
@@ -29,24 +29,61 @@
 %include "include/utils.inc"
 
 ; ===== MACROS ===== ;
+; Keeps track of current round (from 0 to 11)
+%assign round 0
+
+; Assumptions:
+;  - Used for round < 4 (t < 16)
+;  - xmm15 contains shuffle mask
+; Effects:
+;  - Changes %1
+;  - Accesses RDI (message ptr)
+; Arguments:
+;  - register: rotating register
+%macro shani_load 1
+    movdqa          %1, [rdi + 16 * round]
+    pshufb          %1, xmm15
+    shani_round     %1
+
+    %assign         round round + 1
+%endmacro
+
 ; Assumptions:
 ;  - Used for round > 3 (t > 15)
+; Effects:
+;  - Changes xmm8 and %1
 ; Arguments:
 ;  - register: 1st rotating register
 ;  - register: 2nd rotating register
 ;  - register: 3rd rotating register
 ;  - register; 4th rotating register
-%macro update_schedule 4
-    sha256msg1 %1, %2
-	movdqu	xmm8, %4
-	palignr	xmm8, %3, 4
-	paddd	%1, xmm8
-	sha256msg2 %1, %4
+%macro shani_update 4
+    sha256msg1      %1, %2
+	movdqu	        xmm8, %4
+	palignr	        xmm8, %3, 4
+	paddd	        %1, xmm8
+	sha256msg2      %1, %4
+    shani_round     %1
+
+    %assign         round round + 1
+%endmacro
+
+; Effects:
+;  - Changes xmm0, xmm1, xmm2
+; Arguments:
+;  - register: rotating register
+%macro shani_round 1
+    movdqa          xmm0, %1
+    paddd           xmm0, [rel k + 16 * round]
+    sha256rnds2     xmm2, xmm1, xmm0
+    punpckhqdq      xmm0, xmm0
+    sha256rnds2     xmm1, xmm2, xmm0
 %endmacro
 
 section .data
-    ; Hash constants
-    h               dd 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+    ; Hash constants (order 0,1,4,5; 2,3,6,7)
+    h               dd 0x9b05688c, 0x510e527f, 0xbb67ae85, 0x6a09e667
+                    dd 0x5be0cd19, 0x1f83d9ab, 0xa54ff53a, 0x3c6ef372
     ; Round constants
     k               dd 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, \
                        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, \
@@ -93,29 +130,25 @@ libcrpyto_sha256:
 
     movdqa          xmm15, [rel shufmask]
 
-    movdqa          xmm3, [rdi]
-    pshufb          xmm3, xmm15
-    movdqa          xmm4, [rdi + 16]
-    pshufb          xmm4, xmm15
-    movdqa          xmm5, [rdi + 32]
-    pshufb          xmm5, xmm15
-    movdqa          xmm6, [rdi + 48]
-    pshufb          xmm6, xmm15
+    shani_load      xmm3
+    shani_load      xmm4
+    shani_load      xmm5
+    shani_load      xmm6
 
-    update_schedule xmm3, xmm4, xmm5, xmm6
-    update_schedule xmm4, xmm5, xmm6, xmm3
-    update_schedule xmm5, xmm6, xmm3, xmm4
-    update_schedule xmm6, xmm3, xmm4, xmm5
+    shani_update    xmm3, xmm4, xmm5, xmm6
+    shani_update    xmm4, xmm5, xmm6, xmm3
+    shani_update    xmm5, xmm6, xmm3, xmm4
+    shani_update    xmm6, xmm3, xmm4, xmm5
 
-    update_schedule xmm3, xmm4, xmm5, xmm6
-    update_schedule xmm4, xmm5, xmm6, xmm3
-    update_schedule xmm5, xmm6, xmm3, xmm4
-    update_schedule xmm6, xmm3, xmm4, xmm5
+    shani_update    xmm3, xmm4, xmm5, xmm6
+    shani_update    xmm4, xmm5, xmm6, xmm3
+    shani_update    xmm5, xmm6, xmm3, xmm4
+    shani_update    xmm6, xmm3, xmm4, xmm5
 
-    update_schedule xmm3, xmm4, xmm5, xmm6
-    update_schedule xmm4, xmm5, xmm6, xmm3
-    update_schedule xmm5, xmm6, xmm3, xmm4
-    update_schedule xmm6, xmm3, xmm4, xmm5
+    shani_update    xmm3, xmm4, xmm5, xmm6
+    shani_update    xmm4, xmm5, xmm6, xmm3
+    shani_update    xmm5, xmm6, xmm3, xmm4
+    shani_update    xmm6, xmm3, xmm4, xmm5
 
     epilog
     ret
